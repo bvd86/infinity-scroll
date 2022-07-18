35700d637298458a91695bb7be68a932287ed8fe
Billy Veillette-Daigle <95302179+bvd86@users.noreply.github.com>
2022-07-18 10:26:00 -0400

    Added the loader and get 30 picures

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
 let photoArray = [];
+let ready = false
+let imagesLoaded = 0;
+let totalImages= 0;
 
 // Unsplash API
-const count = 10;
+const count = 30;
 const apiKey = 'hry7lEFjfPvqg6IfjS_RKv-isc0GUjqQ55PxRlb4EMU'
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// Check if image were loaded
+const imageLoaded = () => {
+  imagesLoaded ++;
+  if(imagesLoaded === totalImages) {
+    ready = true;
+    loader.hidden = true;
+  }
+}
 
 // Helper Function
 const setAttributes = (element, attributes) => {
@@ -16,25 +28,24 @@ const setAttributes = (element, attributes) => {
 
 // Create Element for Links & Photos
 const displayPhotos = () => {
+  imagesLoaded = 0;
+  totalImages = photoArray.length
   photoArray.forEach((photo) => {
     // Create <a> to link to unsplash
     const item = document.createElement('a');
-    // item.setAttribute('href', photo.links.html);
-    // item.setAttribute('target', '_blank');
     setAttributes(item, {
       href: photo.links.html,
       target: '_blank',
     });
     // Create <img> for photo
     const img = document.createElement('img');
-    // img.setAttribute('src', photo.urls.regular);
-    // img.setAttribute('title', photo.alt_description);
-    // img.setAttribute('alt', photo.alt_description);
     setAttributes(img, {
       src: photo.urls.regular,
       alt: photo.alt_decription,
       title: photo.alt_decription,
-    })
+    });
+    // Event Listener, check when loading
+    img.addEventListener('load', imageLoaded())
     // Put <img> inside <a> and in container
     item.appendChild(img);
     imageContainer.appendChild(item);
@@ -51,6 +62,14 @@ async function getPhotos() {
     // Catch Error Here
   }
 }
+
+// Check scroll for bottom of the page
+window.addEventListener('scroll', () => {
+  if(window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
+    ready = false
+    getPhotos()
+  }
+})
 
 // On Load
 getPhotos();
